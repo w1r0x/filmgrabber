a75f9a7729075b15e7831a00c78fd59b6467398b
w1r0x <vv1r0x@gmail.com>
2024-10-24 16:54:25 +0300

    Add allowed_usernames settings to restrict bot access

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import json
 import telebot
 import logging
 
+
+def check_allowed_usernames(username):
+    return username in allowed_usernames
+
+
 logger = logging.getLogger(__name__)
 log_levels = {'info': logging.INFO, 'debug': logging.DEBUG}
 
@@ -16,15 +21,24 @@ log_level = log_levels[settings['log_level']]
 logging.basicConfig(level=log_level)
 bot = telebot.TeleBot(tg_bot_api_key)
 
+# get allowed usernames from settings file
+allowed_usernames = settings['allowed_usernames']
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    if not check_allowed_usernames(message.from_user.username):
+        bot.send_message(message.from_user.id, "К сожалению у вас нет доступа к боту")
+        return
     logger.debug(f'Received start from "{message.from_user.username}"')
     bot.send_message(message.from_user.id, "👋 Привет! Я скачаю за тебя фильмы и сериалы с торрентов")
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    if not check_allowed_usernames(message.from_user.username):
+        bot.send_message(message.from_user.id, "К сожалению у вас нет доступа к боту")
+        return
     logger.debug(f'Received message from "{message.from_user.username}"')
     bot.send_message(message.from_user.id, 'Пока я ничего не умею, но скоро научусь')
 
