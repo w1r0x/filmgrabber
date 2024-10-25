cb77cb97d2e67737a8c25ee84953352b6a0e92d1
w1r0x <vv1r0x@gmail.com>
2024-10-25 19:15:28 +0300

    Sort movie list by release year

--- myradarr.py
+++ myradarr.py
@@ -6,6 +6,7 @@ def find_movies(radarr, movie_name, no_poster_image_policy='ignore'):
     for m in movies:
         movie = dict()
         movie['title'] = f"{m['title']} ({m['year']})"
+        movie['year'] = m['year']
         # TODO: Make image placeholder for no image movies or just not include this
         try:
             movie['image_url'] = m['images'][0]['remoteUrl']
@@ -24,4 +25,11 @@ def find_movies(radarr, movie_name, no_poster_image_policy='ignore'):
         movie['tmdbId'] = m['tmdbId']
         movies_array.append(movie)
 
+    movies_array = sort_movie_list(movies_array)
+
     return movies_array
+
+
+def sort_movie_list(movies):
+    sorted_movies = sorted(movies, key=lambda x: x['year'], reverse=True)
+    return sorted_movies
