4378347f8932602ba50f69fbff45d0355c73e370
w1r0x <vv1r0x@gmail.com>
2024-10-29 16:29:57 +0300

    Add base module for Kinopoisk.dev API

---
+++ kpapi.py
@@ -0,0 +1,53 @@
+import requests
+import json
+
+
+class MovieNotFoundExc:
+    pass
+
+
+class KPApi:
+
+    api_key = None
+    kinopoisk_dev_url = "https://api.kinopoisk.dev/v1.4"
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    @staticmethod
+    def _get_movie_object(kp_api_response):
+        movie = dict()
+        movie["id"] = kp_api_response["id"]
+        movie["rating"] = kp_api_response["rating"]["kp"]
+        try:
+            movie["trailer"] = kp_api_response["videos"]["trailers"][0]["url"]
+        except KeyError:
+            pass
+
+        return movie
+
+    def _send(self, request):
+        req = self.kinopoisk_dev_url
+        req += request
+
+        headers = {
+            "accept": "application/json",
+            "X-API-KEY": self.api_key
+        }
+
+        response = requests.get(req, headers=headers)
+
+        # TODO: make exception on invalid API key and other errors
+        return json.loads(response.text)
+
+    def find_movie_by_imdb_id(self, imdb_id):
+        request = f"/movie?page=1&limit=1&selectFields=id&selectFields=rating&selectFields=videos&externalId.imdb={imdb_id}"
+
+        response = self._send(request)
+
+        try:
+            movie = response["docs"][0]
+        except KeyError:
+            raise MovieNotFoundExc
+
+        return self._get_movie_object(movie)
